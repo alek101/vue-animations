fa05e952a3e09ceea912b834b41e72bd0f0e67d3
alek101 <47416711+alek101@users.noreply.github.com>
2021-05-12 13:37:18 +0200

    GSAP

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,16 +1,12 @@
 <template>
   <div class="about">
     <transition 
-    appear 
-    name="fade"
+    appear
     @before-enter="beforeEnter"
     @enter="enter"
     @after-enter="afterEnter"
-    @before-leave="beforeLeave"
-    @leave="leave"
-    @after-leave="afterLeave"
     >
-      <h1 v-if="showTitle">About</h1>
+      <h1>About</h1>
     </transition>
     
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
@@ -21,40 +17,32 @@
 
 <script>
 import {ref} from 'vue'
+import gsap from 'gsap'
 
 export default {
   setup() {
-    const showTitle = ref(true)
-
     const beforeEnter = (el) => {
-      console.log('before enter',el)
+      console.log('before enter - set initial state')
+      el.style.transform = 'translateY(-60px)'
+      el.style.opacity = 0
     }
 
-    const enter = (el) => {
-      console.log('enter',el)
+    const enter = (el, done) => {
+      console.log('starting to enter - making transition')
+      gsap.to(el, {
+        duration: 5,
+        y: 0,
+        opacity: 1,
+        ease: 'bounce.out',
+        onComplete: done
+      })
     }
 
     const afterEnter = (el) => {
-      el.style.color="green"
-      console.log('after enter',el)
+      console.log('after enter - done');
     }
 
-    setTimeout(() => showTitle.value=false, 5000)
-
-    const beforeLeave = (el) => {
-      el.style.color="red"
-        console.log('before leave',el)
-      }
-
-      const leave = (el) => {
-        console.log('leave',el)
-      }
-
-      const afterLeave = (el) => {
-        console.log('after leave',el)
-      }
-
-    return {beforeEnter, enter, afterEnter, showTitle, beforeLeave, leave, afterLeave}
+    return {beforeEnter, enter, afterEnter}
   }
 }
 </script>
@@ -63,18 +51,5 @@ export default {
   .about {
     max-width: 600px;
     margin: 20px auto;
-  }
-
-  .fade-enter-from {
-    opacity: 0;
-  }
-  .fade-enter-active {
-    transition: opacity 3s ease;
-  }
-  .fade-leave-to {
-    opacity: 0;
-  }
-  .fade-leave-active {
-    transition: opacity 3s ease;
   }
 </style>
